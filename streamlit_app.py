b78c9e191b2df591112c47e18eef43f9cf7a8c87
claudiaadmin <162459056+claudiaadmin@users.noreply.github.com>
2024-03-07 12:22:47 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,11 @@ st.write("""
 # My first app
 Hello *world!*
 """)
+
+
+def upload_file():
+    uploaded_file = st.file_uploader("Upload a file!")
+    if uploaded_file is not None:
+        st.session_state.filename = uploaded_file.name
+        with open(os.path.join("documents", uploaded_file.name), "wb") as f:
+            f.write(uploaded_file.getbuffer())
